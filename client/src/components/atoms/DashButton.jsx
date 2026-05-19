3648ea75dd41b2a28cfdbf203451e906d4f230f1
DorbanE <santiago.londonob.col@upb.edu.co>
2026-05-19 16:34:54 -0500

    fix: slight change in width for better viewing

--- client/src/components/atoms/DashButton.jsx
+++ client/src/components/atoms/DashButton.jsx
@@ -3,7 +3,7 @@ export default function QuickAccessItem({ id, label, icon, onClick }) {
 <button
   onClick={() => onClick?.(id)}
   aria-label={label}
-  className='bg-rosa-menu rounded-xl py-5 px-2 flex flex-col items-center gap-6 shadow-sm hover:shadow-md w-80 duration-300 hover:-translate-y-0.5 cursor-pointer'
+  className='bg-rosa-menu rounded-xl py-5 px-2 flex flex-col items-center gap-6 shadow-sm hover:shadow-md w-full duration-300 hover:-translate-y-0.5 cursor-pointer'
 >
       {icon}
       <span className="text-xs font-bold text-gray-800 ">{label}</span>
